0683ec653b15a52b17d5a6d1c62442834db9210e
andreas.imboden <andreas.imboden@europa3000.ch>
2020-10-12 16:56:42 +0200

    Manual.Test .Net Core

--- StringKing.Manual.Test/Program.cs
+++ StringKing.Manual.Test/Program.cs
@@ -1,12 +1,7 @@
-﻿using StringKing.FunctionInterface;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using StringKing;
+﻿using System;
+using StringKing.FunctionInterface;
 
-namespace StringKing.ManualTest
+namespace StringKing.Manual.Test
 {
     class Program
     {
